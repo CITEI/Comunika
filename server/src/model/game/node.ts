6f51928e3846c87875c3595fe16cec5c7ac296e6
nickolasrm <nickolasrochamachado@gmail.com>
2022-05-06 22:54:28 -0300

    fixed task discriminator

--- server/src/model/game/node.ts
+++ server/src/model/game/node.ts
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
-export interface INode {
+/*
+Creating regular nodes
+Regular nodes are a specific type of node used for displaying
+data to the user
+ */
+export interface NodeInput {
   title: string;
 }
 
@@ -8,4 +13,53 @@ export const NodeSchemaInit = {
   title: { type: String, required: true, minlength: 2 },
 };
 
-export const NodeSchema = new mongoose.Schema<INode>(NodeSchemaInit);
+const NO_ID = {_id: false}
+
+export const NodeSchema = new mongoose.Schema(NodeSchemaInit, {
+  discriminatorKey: "type",
+  ...NO_ID
+});
+
+/*
+Creating regular node children
+The nodes declared below are similar to concrete classes of Node
+*/
+const TextNodeSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+}, NO_ID);
+const ImageNodeSchema = new mongoose.Schema({
+  imageAlt: { type: String, required: false },
+  imageUrl: { type: String, required: true },
+}, NO_ID);
+export const NodeDiscriminators = {
+  Text: TextNodeSchema,
+  Image: ImageNodeSchema
+}
+
+/*
+Creating question nodes
+Question nodes, similarly to nodes, are a kind of node that not only focuses
+on displaying data, but also on the answer evaluation
+*/
+export interface QuestionNodeInput extends NodeInput {
+  question: string;
+}
+
+export const QuestionNodeSchemaInit = {
+  ...NodeSchemaInit,
+  question: { type: String, required: true, minlength: 3 },
+};
+
+export const QuestionNodeSchema = new mongoose.Schema(QuestionNodeSchemaInit, {
+  discriminatorKey: "type",
+  ...NO_ID
+});
+
+/*
+Creating question node children
+The nodes declared below are similar to concrete classes of QuestionNode
+*/
+const YesNoQuestionNodeSchema = new mongoose.Schema({}, NO_ID);
+export const QuestionNodeDiscriminators = {
+  YesNo: YesNoQuestionNodeSchema,
+}
--- server/src/model/game/task.ts
+++ server/src/model/game/task.ts
@@ -1,19 +1,42 @@
-import { NodeSchema, INode } from "./node";
+import {
+  NodeSchema,
+  NodeInput,
+  QuestionNodeInput,
+  QuestionNodeSchema,
+  NodeDiscriminators,
+  QuestionNodeDiscriminators,
+} from "./node";
 import mongoose from "mongoose";
 
-export interface ITask extends mongoose.Document {
+export interface TaskInput {
   name: string;
   description: string;
-  nodes: INode[];
+  nodes: Array<{ type: string } & NodeInput & { [key: string]: any }>;
+  answer_nodes: Array<
+    { type: string } & QuestionNodeInput & { [key: string]: any }
+  >;
 }
 
-export const TaskSchema = new mongoose.Schema<ITask>({
+export const TaskSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 2 },
-  description: { type: String, required: false },
+  description: { type: String, required: true },
   nodes: [{ type: NodeSchema, required: true, minlength: 1 }],
+  question_nodes: [{ type: QuestionNodeSchema, required: true, minlength: 1 }],
 });
 
-export const NodesField =
-  TaskSchema.path<mongoose.Schema.Types.Subdocument>("nodes");
+/*
+Registering discriminators
+The section below allow us to simulate node inheritance into the db
+ */
+const Nodes = TaskSchema.path<mongoose.Schema.Types.Subdocument>("nodes");
+for (const [name, schema] of Object.entries(NodeDiscriminators))
+  Nodes.discriminator(name, schema);
 
-export const Task = mongoose.model("Task", TaskSchema);
+const QuestionNodes =
+  TaskSchema.path<mongoose.Schema.Types.Subdocument>("question_nodes");
+for (const [name, schema] of Object.entries(QuestionNodeDiscriminators))
+  QuestionNodes.discriminator(name, schema);
+
+export interface TaskDocument extends mongoose.Document, TaskInput {}
+
+export const Task = mongoose.model<TaskDocument>("Task", TaskSchema);
