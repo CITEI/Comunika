24ed3046f47e4ff972ebbf9953f6d87c324fbc03
nickolasrm <nickolasrochamachado@gmail.com>
2022-05-07 22:35:48 -0300

    enabled winston verbose development

--- server/src/pre-start/winston.ts
+++ server/src/pre-start/winston.ts
@@ -3,25 +3,35 @@ import path from "path";
 import winston from "winston";
 import { LOG_FOLDER } from "./constants";
 
+const date = moment().format("YYYY-MM-DD");
 
-const date = moment().format('YYYY-MM-DD');
-
-export default winston.createLogger({
+const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.errors({ stack: true }),
-    winston.format.colorize(),
-    winston.format.json()
+    winston.format.prettyPrint()
   ),
   transports: [
     new winston.transports.File({
       filename: path.join(LOG_FOLDER, "error", `error-${date}.log`),
       level: "error",
+      format: winston.format.json(),
     }),
     new winston.transports.File({
       filename: path.join(LOG_FOLDER, "info", `info-${date}.log`),
       level: "info",
+      format: winston.format.json(),
     }),
   ],
 });
+
+if (process.env.NODE_ENV == "development") {
+  logger.add(
+    new winston.transports.Console({
+      level: "info",
+    })
+  );
+}
+
+export default logger;
