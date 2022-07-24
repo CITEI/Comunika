5282cd3ccb3d8e4b28a19e2b725b9743e6b8af3d
nickolasrm <nickolasrochamachado@gmail.com>
2022-07-23 22:49:09 -0300

    styled login view

--- client/App.tsx
+++ client/App.tsx
@@ -12,39 +12,45 @@ import Game from "./src/view/game";
 import Result from "./src/view/result";
 import { APP_NAME } from "./src/pre-start/constants";
 import Settings, { SettingsButton } from "./src/view/settings";
+import { defaultTheme } from "./src/pre-start/themes";
+import { ThemeProvider } from "styled-components/native";
+import FontLoader from "./src/pre-start/font-loader";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen
-            component={Login}
-            name="Login"
-            options={{ title: APP_NAME, headerLeft: () => null }}
-          />
-          <Stack.Screen
-            component={Register}
-            name="Register"
-            options={{ title: APP_NAME }}
-          />
-          <Stack.Screen
-            component={Main}
-            name="Main"
-            options={{
-              title: APP_NAME,
-              headerRight: SettingsButton,
-            }}
-          />
-          <Stack.Screen component={Categories} name="Categories" />
-          <Stack.Screen component={Game} name="Game" />
-          <Stack.Screen component={Result} name="Result" />
-          <Stack.Screen component={Settings} name="Settings" />
-        </Stack.Navigator>
-      </NavigationContainer>
-      <FlashMessage position="bottom" />
+      <FontLoader />
+      <ThemeProvider theme={defaultTheme}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen
+              component={Login}
+              name="Login"
+              options={{ title: APP_NAME, headerShown: false }}
+            />
+            <Stack.Screen
+              component={Register}
+              name="Register"
+              options={{ title: APP_NAME }}
+            />
+            <Stack.Screen
+              component={Main}
+              name="Main"
+              options={{
+                title: APP_NAME,
+                headerRight: SettingsButton,
+              }}
+            />
+            <Stack.Screen component={Categories} name="Categories" />
+            <Stack.Screen component={Game} name="Game" />
+            <Stack.Screen component={Result} name="Result" />
+            <Stack.Screen component={Settings} name="Settings" />
+          </Stack.Navigator>
+        </NavigationContainer>
+        <FlashMessage position="bottom" />
+      </ThemeProvider>
     </Provider>
   );
 }
--- client/src/view/login.tsx
+++ client/src/view/login.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import LoginForm from "../component/organism/login-form";
 import VerticalContainer from "../component/atom/vertical-container";
 import { useAppDispatch } from "../store/store";
 import { login } from "../store/auth";
 import Button from "../component/atom/button";
-import { showMessage } from "react-native-flash-message";
 import { FullView } from "../component/atom/full-view";
 import { useSelector } from "react-redux";
 import { useNavigation } from "@react-navigation/native";
 import { AuthNavigatorProps } from "../route/auth";
-import { Text } from "react-native";
+import Title from "../component/atom/title";
+import { Image, View } from "react-native";
+import logo from "../../assets/logo.png";
+import { dp } from "../helper/resolution";
+import Text from "../component/atom/text";
+import Modal from "../component/molecule/modal";
 
 export interface LoginProps {}
 
@@ -19,24 +23,14 @@ const Login: React.VoidFunctionComponent<LoginProps> = (props) => {
   const authStatus = useSelector((state) => (state as any).auth.status);
 
   const [first, setFirst] = useState(true);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     if (!first) {
-      if (authStatus.isAuthenticated)
-        showMessage({
-          type: "success",
-          message: "Logged",
-          onHide: () => {
-            navigation.popToTop();
-            navigation.navigate("Main");
-          },
-        });
-      else
-        showMessage({
-          type: "danger",
-          description: "Invalid credentials",
-          message: "Not logged",
-        });
+      if (authStatus.isAuthenticated) {
+        navigation.popToTop();
+        navigation.navigate("Main");
+      } else setModalVisible(true);
     } else setFirst(false);
   }, [authStatus]);
 
@@ -51,15 +45,41 @@ const Login: React.VoidFunctionComponent<LoginProps> = (props) => {
   };
 
   return (
-    <FullView>
-      <Text style={{ fontWeight: "bold" }}>Login</Text>
+    <FullView style={{ paddingHorizontal: dp(20) }}>
+      <Modal
+        text="Invalid credentials are present!"
+        title="Not logged! :("
+        onRequestClose={useCallback(() => setModalVisible(false), [])}
+        visible={modalVisible}
+      ></Modal>
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          marginBottom: dp(25),
+          marginTop: dp(25),
+        }}
+      >
+        <Image
+          source={logo}
+          style={{
+            width: dp(98),
+            height: dp(41),
+            marginBottom: dp(39),
+          }}
+        ></Image>
+        <Title>Welcome!</Title>
+      </View>
       <VerticalContainer>
         <LoginForm onSubmit={handleLogin} />
         <Button
-          title="Register"
-          style={{ marginTop: "5px" }}
+          title="Create account"
           onPress={handleRegister}
+          variant="outline"
         />
+        <Text style={{ marginTop: 20, textAlign: "center" }}>
+          Forgot the password? Click here
+        </Text>
       </VerticalContainer>
     </FullView>
   );
