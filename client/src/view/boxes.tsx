1422ed274f03f920dd6088013ddfde3e6caed1b1
nickolasrm <nickolasrochamachado@gmail.com>
2022-08-03 06:07:42 -0300

    transformed boxes screen into a loading screen

--- client/src/view/boxes.tsx
+++ client/src/view/boxes.tsx
@@ -1,49 +1,36 @@
 import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../store/store";
-import { BoxItem, fetchBoxes } from "../store/game-data";
+import { fetchBoxes } from "../store/game-data";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { GameNavigatorProps } from "../route/game";
-import { showMessage } from "react-native-flash-message";
-import Cards from "../component/templates/cards";
+import MainContainer from "../component/atom/main-container";
 
 interface BoxesProps {}
 
+/** Non visual screen to load boxes */
 const Boxes: React.VoidFunctionComponent<BoxesProps> = (props) => {
   const route = useRoute();
   const { stageId } = route.params as any;
   const navigation = useNavigation<GameNavigatorProps>();
   const dispatch = useAppDispatch();
+  const stages = useAppSelector((state) => state.gameData.stages.data);
   const boxes = useAppSelector((state) => state.gameData.boxes);
-  const box = useAppSelector((state) => state.user.progress.box);
+  const boxId = useAppSelector((state) => state.user.progress.box);
 
   useEffect(() => {
     if (!(stageId in boxes)) dispatch(fetchBoxes(stageId));
   }, []);
 
-  const handleItemPress = (item: BoxItem) => {
-    if (box == item._id)
-      navigation.navigate("Transition", { box: item });
-    else
-      showMessage({
-        message: "Not your current box",
-        type: "info",
-      });
-  };
+  useEffect(() => {
+    if (boxes && stageId in boxes && stages) {
+      const boxIndex = boxes[stageId].findIndex((el) => el._id == boxId);
+      const stage = stages.find((el) => el._id == stageId);
+      if (boxIndex > -1 && stage)
+        navigation.navigate("Transition", { boxIndex: boxIndex + 1, stage });
+    }
+  }, [boxes, stages]);
 
-  return (
-    <>
-      {boxes[stageId] ? (
-        <Cards
-          title="Boxes"
-          onPress={handleItemPress}
-          data={boxes[stageId]}
-          current={0}
-        />
-      ) : (
-        <></>
-      )}
-    </>
-  );
+  return <MainContainer></MainContainer>;
 };
 
 export default Boxes;
