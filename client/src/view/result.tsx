8358d56d4b07e5786143dc521b6043e800ea2549
nickolasrm <nickolasrochamachado@gmail.com>
2022-09-17 00:39:20 -0300

    added more info at result screen
    Signed-off-by: nickolasrm <nickolasrochamachado@gmail.com>

--- client/src/view/result.tsx
+++ client/src/view/result.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ResultTemplate from "../component/templates/result";
-import { useAppSelector } from "../store/store";
+import { fetchStages, StageItem } from "../store/game-data";
+import { useAppDispatch, useAppSelector } from "../store/store";
+import { fetchUserData } from "../store/user";
 
+/** Screen displayed after an activity box is completed */
 const Result: React.VoidFunctionComponent = () => {
+  const dispatch = useAppDispatch();
+  const userReloaded = useAppSelector((state) => state.user.loaded);
+
+  useEffect(() => {
+    if (!userReloaded) dispatch(fetchUserData());
+  }, [userReloaded]);
+
+  const moduleStages = useAppSelector((state) => state.gameData.stages);
   const moduleId = useAppSelector((state) => state.user.progress.module);
-  const modules = useAppSelector((state) => state.gameData.modules.data);
+  const stageId = useAppSelector((state) => state.user.progress.stage);
+  const [stage, setStage] = useState<undefined | StageItem>(undefined);
 
-  const module = modules.find((module) => module._id == moduleId);
+  useEffect(() => {
+    if (userReloaded && moduleId && stageId)
+      if (moduleId in moduleStages)
+        setStage(moduleStages[moduleId].find((stage) => stage._id == stageId));
+      else dispatch(fetchStages(moduleId));
+  }, [moduleId, stageId, moduleStages, userReloaded]);
 
-  return module ? <ResultTemplate module={module} /> : <></>;
+  return stage ? <ResultTemplate stage={stage} /> : <></>;
 };
 
 export default Result;
