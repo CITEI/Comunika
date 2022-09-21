26e4935a2d8a2ee11391fa834fafa83e22d064a9
nickolasrm <nickolasrochamachado@gmail.com>
2022-09-21 00:54:03 -0300

    updated game to avoid state synchronization problems
    Signed-off-by: nickolasrm <nickolasrochamachado@gmail.com>

--- client/src/view/game.tsx
+++ client/src/view/game.tsx
@@ -1,49 +1,42 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "../store/store";
-import { evaluate, fetchBox } from "../store/user";
+import { useAppDispatch } from "../store/store";
+import { evaluate } from "../store/user";
 import { useNavigation } from "@react-navigation/native";
 import { GameNavigatorProps } from "../route/game";
 import Activity from "../component/templates/activity";
+import useUserModule from "../hooks/useusermodule";
+import useBox from "../hooks/usebox";
 
 interface GameProps {}
 
+/** Main game screen */
 const Game: React.VoidFunctionComponent<GameProps> = () => {
   const navigation = useNavigation<GameNavigatorProps>();
   const dispatch = useAppDispatch();
-  const box = useAppSelector((state) => state.user.box);
-  const boxLoaded = useAppSelector((state) => state.user.boxLoaded);
-  const evaluation = useAppSelector((state) => state.user.result);
+  const box = useBox();
+  const module = useUserModule();
+  const [answers, setAnswers] = useState<boolean[][]>([]);
 
-  const moduleId = useAppSelector((state) => state.user.progress.module);
-  const modules = useAppSelector((state) => state.gameData.modules.data);
-
-  const module = modules.find((module) => module._id == moduleId);
-
-  const [evaluated, setEvaluated] = useState(false);
-  const [finished, setFinished] = useState(false);
-
-  useEffect(() => {
-    if (!boxLoaded) dispatch(fetchBox());
-  }, [boxLoaded]);
-
-  /** Sends answers to evaluation */
+  /** Saves answers for sending */
   const handleFinish = useCallback((answers: boolean[][]) => {
-    dispatch(evaluate(answers));
-    setFinished(true);
+    setAnswers(answers);
   }, []);
 
+  /** Sends answers to the api */
   useEffect(() => {
-    if (!evaluated)
-      setEvaluated(true);
-    else if (evaluated && finished)
-      navigation.navigate("Result");
-  }, [evaluation, evaluated, finished]);
+    if (answers.length > 0) dispatch(evaluate(answers));
+  }, [answers]);
 
-  return boxLoaded ? (
+  /** Navigates to the next screen after sending answers */
+  useEffect(() => {
+    if (answers.length > 0) navigation.replace("Result");
+  }, [box]);
+
+  return box && module ? (
     <Activity
-      activities={box || []}
+      activities={box}
       onFinish={handleFinish}
-      module={module?.name || "None"}
+      module={module.name}
     ></Activity>
   ) : (
     <></>
