177de4ddaf0239f7ea37d0d92ed7dccf6d2fa3c9
cesarzxk <cesar3d.vargas@gmail.com>
2023-01-22 16:33:33 -0300

    feat: change user game answers prop

--- server/src/route/game/user.ts
+++ server/src/route/game/user.ts
@@ -1,4 +1,4 @@
-import { EvaluationStatus, userService } from "../../service/user";
+import { EvaluationStatus, userService } from "../../service/code";
 import { celebrate, Joi } from "celebrate";
 import { Router, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
@@ -10,24 +10,24 @@ const router = Router();
 
 router.use(passport.authenticate("jwt", { session: false }));
 
-router.get(
-  "/box",
-  async (req: Request, res: Response) => {
-    const id = (req.user as UserDocument)._id;
-    const box = await userService.findBox({ id });
-    res.status(StatusCodes.OK).send(box);
-  }
-);
+router.get("/box", async (req: Request, res: Response) => {
+  const id = (req.user as UserDocument)._id;
+  const box = await userService.findBox({ id });
+
+  return res.status(StatusCodes.OK).send(box).end();
+});
 
 router.post(
   "/box",
   celebrate({
     body: {
-      answers: Joi.array().items(Joi.array().items(Joi.boolean())).required(),
+      answers: Joi.array().items(Joi.array().items(Joi.object())).required(),
     },
   }),
   async (req: Request, res: Response) => {
+    console.log("box");
     const id = (req.user as UserDocument)._id;
+
     switch (+(await userService.evaluate({ id, ...req.body }))) {
       case EvaluationStatus.Approved:
         res.status(StatusCodes.OK).send({ status: "approved" });
@@ -41,23 +41,15 @@ router.post(
   }
 );
 
-router.get(
-  "/history",
-  async (req: Request, res: Response) => {
-    const id = (req.user as UserDocument)._id;
-    const history = await userService.findHistory({ id });
-    res.status(StatusCodes.OK).send(history);
-  }
-);
+router.get("/history", async (req: Request, res: Response) => {
+  const id = (req.user as UserDocument)._id;
+  const history = await userService.findHistory({ id });
+  res.status(StatusCodes.OK).send(history);
+});
 
-router.get(
-  "/",
-  async (req: Request, res: Response) => {
-    const id = (req.user as UserDocument)._id;
-    res
-      .status(StatusCodes.OK)
-      .send(await userService.findUserData({ id }));
-  }
-);
+router.get("/", async (req: Request, res: Response) => {
+  const id = (req.user as UserDocument)._id;
+  res.status(StatusCodes.OK).send(await userService.findUserData({ id }));
+});
 
 export default router;
