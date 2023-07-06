eef9942911b744a4a9db42fb5b4ae0c9fad992d7
Davi Oliveira <davicicfaculdade@gmail.com>
2023-07-06 08:33:41 -0300

    Adicionando criação de dois tipos de usuário.

---
+++ server/src/model/parent.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import { User, UserInput } from './user';
+import { MIN_STRING_LENGTH } from '../pre-start/constants';
+
+export interface ParentInput extends UserInput {
+  relationship: string,
+  birth: Date,
+  region: string,
+}
+
+const Parent = User.discriminator('Parent', new mongoose.Schema({
+  relationship: {
+    type: String,
+    required: true,
+    minlength: MIN_STRING_LENGTH,
+  },
+  birth: {
+    type: Date,
+    required: true
+  },
+  region: {
+    type: String,
+    required: true,
+    minlength: MIN_STRING_LENGTH
+  },
+}));
+
+export default Parent;
--- server/src/route/utils/custom_joi.ts
+++ server/src/route/utils/custom_joi.ts
@@ -4,6 +4,7 @@ import { UploadFlags } from "../admin/utils/functions"
 
 export const CustomJoi = {
   ObjectId: () => Joi.string().length(24),
+  String: () => Joi.string().min(MIN_STRING_LENGTH),
   RequiredString: () => Joi.string().min(MIN_STRING_LENGTH).required(),
   UploadStatus: () => Joi.string().invalid(UploadFlags.invalid),
 }
